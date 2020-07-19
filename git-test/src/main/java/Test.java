353c7a9923b2fde26a89f4bacfe4700ccfe21bee
zhjw <1916168788@qq.com>
2020-07-20 00:50:34 +0800

    本地更新

--- git-test/src/main/java/Test.java
+++ git-test/src/main/java/Test.java
@@ -6,6 +6,6 @@ public class Test {
         System.out.println("創建分支");
         System.out.println("主幹添加");
         System.out.println("分支添加");
-        System.out.println("在线更新");
+        System.out.println("本地更新");
     }
 }
